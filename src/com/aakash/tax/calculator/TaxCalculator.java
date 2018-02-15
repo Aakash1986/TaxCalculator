ca31c6f922c31b26701732475abe0e3aa31df907
Aakash1986 <sharma.aakash17@gmail.com>
2018-02-16 00:49:28 +0100

    JavaDoc Comments added

--- src/com/aakash/tax/calculator/TaxCalculator.java
+++ src/com/aakash/tax/calculator/TaxCalculator.java
@@ -3,6 +3,17 @@ package com.aakash.tax.calculator;
 import java.math.BigDecimal;
 
 public interface TaxCalculator {
+    /**
+     * ordinaryIncomeTax function is used to calculate Income tax which is 23% of total Income earned Monthly.
+     * @param income Income Earned on Monthly Basis.
+     * @return Returns Ordinary Income tax payable by User
+     */
     BigDecimal ordinaryIncomeTax( BigDecimal income);
+
+    /**
+     * socialSecurityTax function is used to calculate social security tax which is 8.2% of Income and max of 25% of Income Exceeding 54650 kr
+     * @param income Income Earned on Monthly Basis.
+     * @return Returns Social Security tax payable by User
+     */
     BigDecimal socialSecurityTax( BigDecimal income);
 }
--- src/com/aakash/tax/data/DataValidator.java
+++ src/com/aakash/tax/data/DataValidator.java
@@ -3,6 +3,11 @@ package com.aakash.tax.data;
 import java.math.BigDecimal;
 
 public class DataValidator {
+
+    /**
+     * Validate method will validate input provided by users.
+     * @param income Income Earned on Monthly Basis.
+     */
     public void validate(BigDecimal income){
         if(income.compareTo(BigDecimal.ZERO) < 0 ){
             throw new IllegalArgumentException("Only Positive Number allowed");
